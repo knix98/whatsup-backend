6c117d48c7f61a76e853c727c17f4ab0048a58e0
Nikhil <ns345360@gmail.com>
2023-01-26 17:59:09 +0530

    coded controller for returning the list of posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,28 @@
-module.exports.posts = function (req, res) {
-  return res.end("<h1>Express is up for whatsup</h1>");
+const Post = require("../models/post");
+
+module.exports.posts = async function (req, res) {
+  try {
+    let posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .limit(req.query.limit)
+      .populate("user", "name")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+          select: "name",
+        },
+      });
+
+    return res.status(200).json({
+      message: "List of posts",
+      success: true,
+      data: posts,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
